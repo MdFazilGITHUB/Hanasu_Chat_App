f25c8a2d5a11a3eec0ccdf8b228ab1174c6ab82e
MdFazilGITHUB <mmilyas540@gmail.com>
2025-08-13 22:05:33 +0530

    server/auth routes completed

--- server/src/index.js
+++ server/src/index.js
@@ -1,21 +1,24 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+dotenv.config();
 
-dotenv.config()
-
-import authRoutes from './routes/auth.route.js';
-import { connectDB } from './lib/db.js';
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.use("/api/auth/", authRoutes)
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth/", authRoutes);
 
 app.get("/", (req, res) => {
-    res.send("API is running...")
-})
+  res.send("API is running...");
+});
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
-    connectDB()
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
